55c7c5d653d2bc533822f817441d21c95d226612
AlenLong <alenlong1@gmail.com>
2023-01-28 21:32:16 -0300

    register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,8 @@
+const createError = require('http-errors');
+const User = require('../database/models/User');
+const errorResponse = require('../helpers/errorResponse');
+const generateToken = require('../helpers/generateToken');
+
 module.exports = {
     register: async (req, res) => {
         try {
@@ -5,21 +10,33 @@ module.exports = {
             const {name, email, password} = req.body;
 
             if([name, email, password].includes('')){
-                let error = new Error('Todos los campos son obligatorios')
-                error.status = 400;
-                throw error
+                throw createError(400,'Todos los campos son obligatorios')
             }
+
+                let user = await User.findOne({
+                    email 
+                });
+
+                
+                if(user){
+                    throw createError(400,'El email ya se encuentra registrado')
+                }
+                
+                user = new User(req.body)
+                user.token = generateToken();
+
+                const userStore = await user.save();
+
+                //TODO: enviar el email de confirmacion
+
 
             return res.status(201).json({
                 ok: true,
                 msg: "Usuario registrado",
-            });
+                data: userStore
+            })
         } catch (error) {
-            console.log(error);
-            return res.status(error.status || 500).json({
-                ok: false,
-                msg: error.message || "Error! Problemas en el Registro",
-            });
+            return errorResponse(res,error, "REGISTER")
         }
     },
 
